2d2f0a88da1cc1a9b8c863d7181795accf654cf2
Joshua Crowe <jcrowe120485@gmail.com>
2021-03-19 18:33:22 -0700

    complete routing section

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,7 +8,7 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
     }
 
     componentDidMount() {
@@ -23,6 +24,7 @@ class NewPost extends Component {
         axios.post('/posts/', post)
             .then(response => {
                 console.log(response)
+                this.setState({submitted: true})
             });
     }
 
